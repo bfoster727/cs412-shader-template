dc01df048dd82acdb17338b312f72beb058ed6f3
bfoster727 <bfoster@plu.edu>
2024-04-28 21:07:36 -0700

    Made particles face camera, added orbit camera

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@
 // These import statements get the source code for the shaders and store them in variables.
 import vertShaderSource from './shader/hello.vert.glsl';
 import fragShaderSource from './shader/hello.frag.glsl';
+import {OrbitControls} from "three/examples/jsm/controls/OrbitControls.js";
 
 import * as THREE from 'three';
 import {GUI} from 'lil-gui';
@@ -45,28 +46,16 @@ function main() : void {
     scene = new THREE.Scene();
     camera = new THREE.PerspectiveCamera( 45.0, canvas.width / canvas.height, 0.5, 2000 );
     camera.position.z = 15;
-
-
-    // Create geometry for a mesh containing a single triangle
-    //const pos = [ -3, -3, 0, 3, -3, 0, 0, 3, 0];
-    //const posBuf = new THREE.BufferAttribute(Float32Array.from(pos), 3);
-    //const geom = new THREE.BufferGeometry();
-    const pos = [0,0,0];
-    const posBuf = new THREE.BufferAttribute(Float32Array.from(pos),1);
-    const geom = new THREE.CircleGeometry(1,32);
-    //geom.setAttribute("position", posBuf);
-
+    
+    
     // The material for the triangle will be our shader program
     const material = new THREE.ShaderMaterial( { 
         vertexShader: vertShaderSource,
         fragmentShader: fragShaderSource 
     } );
     
-    // Add the triangle mesh to the scene
-    //const circle = new THREE.Mesh(geom, material);
-    //circle.position.set(0,2,-10);
-    //scene.add(circle);
-
+    const controls = new OrbitControls(camera, canvas);
+    
     resize();  // Call directly once to initially size the canvas
     window.requestAnimationFrame(draw);
 }
@@ -93,6 +82,7 @@ function update() : void{
             });
             const circle = new THREE.Mesh(geom, material);
             circle.position.set(p.position.x, p.position.y, p.position.z);
+            circle.lookAt(camera.position);
             scene.add(circle);
             p.mesh = circle;
             particles.push(p);
